dcd32d71832038930cbfa08a8cd1c81931a02951
Muhammad-azeem11 <muhammadazeemzahid1>
2026-06-26 18:34:31 +0500

    Portfolio v2.0: Live homepage with Bio, Skills, Experience, Projects, Education

--- bio/views.py
+++ bio/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Bio
+from education.models import Education
+from experience.models import Experience 
+from projects.models import Project
+from skills.models import Skill
 
 def home(request):
-    return HttpResponse('Bio app working')
+    context = {
+        'bio': Bio.objects.first(),
+        'education': Education.objects.all(),
+        'experience': Experience.objects.all(),
+        'projects': Project.objects.all(),
+        'skills': Skill.objects.all(),
+    }
+    return render(request, 'index.html', context)
